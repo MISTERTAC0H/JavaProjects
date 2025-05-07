d4720df8e764ce9761f3696a4b9f02e386cd3575
MISTERTAC0H <evan.guard@icloud.com>
2025-05-07 16:19:01 -0400

    test commit

---
+++ main.java
@@ -0,0 +1,8 @@
+import javax.swing.JOptionPane;
+
+public class main {
+    public static void main(String[] args) {
+        // Show a simple popup message
+        JOptionPane.showMessageDialog(null, "Hello! This is a popup message.");
+    }
+}
